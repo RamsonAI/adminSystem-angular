335995095de2f75b32ba478b2de9adb275d97fe8
RamsonAI <callmekali18@gmail.com>
2024-09-04 21:12:18 +0300

    add user component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,20 @@ import { DashboardComponent } from './interfaces/auth/dashboard/dashboard.compon
 import { RegisterComponent } from './interfaces/auth/register/register.component';
 import { ForgotPasswordComponent } from './interfaces/auth/forgot-password/forgot-password.component';
 import { AdminLayoutComponent } from './interfaces/auth/admin-layout/admin-layout.component';
+import { UsersComponent } from './interfaces/auth/users/users.component';
+import { authGuard } from './interfaces/auth/guards/auth.guard';
 
 export const routes: Routes = [
     {path:"", redirectTo:'auth/login', pathMatch:'full'},
     {path:'auth/login', component:LoginComponent},
     {path:'auth/register', component:RegisterComponent},
     {path:'auth/change-password', component:ForgotPasswordComponent},
-    {path:'auth/admin', component:AdminLayoutComponent,
+    {
+        path:'dashboard', component:AdminLayoutComponent, 
+        canActivate : [authGuard],
         children: [
             {path:'', component:DashboardComponent},
+            {path:'users', component:UsersComponent},
         ]
     },
     {path:'**', redirectTo:'auth/login'}
--- src/app/interfaces/auth/guards/auth.guard.ts
+++ src/app/interfaces/auth/guards/auth.guard.ts
@@ -1,5 +1,12 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const token = localStorage.getItem("token");
+
+  // if(token){
+  //   return true;
+  // }
+  
   return true;
 };
